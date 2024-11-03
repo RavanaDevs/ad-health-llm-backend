97b18daa97035269aa618055743ed4488773ce9a
Kavindu Sanjula <123143684+RavanaDevs@users.noreply.github.com>
2024-11-03 21:10:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS, cross_origin
 from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 llm = ChatGroq(
     temperature=0,
@@ -16,23 +18,38 @@ def index():
     return render_template('index.html')
 
 @app.route('/generate_titles', methods=['POST'])
+@cross_origin()
 def generate_titles():
     data = request.json
-    location = data.get('location')
-    category = data.get('category')
+
+    title_generation_prompt = '''
+Generate five enhanced and engaging titles for a property listing to attract potential buyers or renters. Use the following parameters to improve the existing title provided:
+
+- Title: {title}
+- Description: {description}
+- Price: {price}
+- Search Keywords: {search_keywords}
+
+Each generated title should be clear, concise, and make the property appealing for online searches. Tailor the titles to highlight unique features and competitive advantages related to the description, price, and keywords, maximizing searchability and appeal for potential customers. 
+
+Give only titles. do not provide any other details or descriptions. Do not number the titles and add additional characters from the beginig and end.
+'''
+
+    title = data.get('title')
+    description = data.get('description')
     price = data.get('price')
-    city = data.get('city')
+    search_keywords = data.get('search_keywords')
 
     prompt_extract = PromptTemplate(
-        input_variables=["Location", "category", "price", "city"],
-        template="Give me 5 titles for the description using five words or below: \"{Location}, {city}, {price}, {category}\""
+        input_variables=["title", "description", "price", "search_keywords"],
+        template=title_generation_prompt
     )
 
     formatted_prompt = prompt_extract.format(
-        Location=location,
-        category=category,
+        title=title,
+        description=description,
         price=price,
-        city=city
+        search_keywords=search_keywords
     )
 
     response = llm.invoke(formatted_prompt)
